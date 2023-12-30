3008966a0b8c9aeead79cb0ed5d60c810239546c
aTreeFrog <shyonbabazadeh@gmail.com>
2023-12-29 22:04:29 -0800

    works

--- src/pages/index.js
+++ src/pages/index.js
@@ -256,7 +256,7 @@ export default function Home() {
               <div key={index} className={`flex ${message.type === 'user' ? 'justify-end' : 'justify-start'
                 }`}>
                 <div className={`${message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
-                  } rounded-lg p-4 text-white max-w-sm`}>
+                  } rounded-lg p-2 text-white max-w-sm`}>
                   {message.message}
                 </div>
               </div>
