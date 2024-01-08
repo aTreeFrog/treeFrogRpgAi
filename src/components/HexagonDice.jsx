1dd41206b9c3bcb54c03f4d4e9c183e8fb6d2d93
aTreeFrog <shyonbabazadeh@gmail.com>
2024-01-07 18:08:46 -0800

    good end point

--- src/components/HexagonDice.jsx
+++ src/components/HexagonDice.jsx
@@ -8,9 +8,12 @@ const HexagonDice = () => {
     const [isD20Spinning, setIsD20Spinning] = useState(false); // Initialize spinning state
     const [isD6Spinning, setIsD6Spinning] = useState(false);
     const [isD4Spinning, setIsD4Spinning] = useState(false);
-    const [d20Active, setD20Active] = useState(true);
+    const [d20Active, setD20Active] = useState(false);
     const [d6Active, setD6Active] = useState(false);
-    const [d4Active, setD4Active] = useState(true);
+    const [d4Active, setD4Active] = useState(false);
+    const [d20GlowActive, setD20GlowActive] = useState(false);
+    const [d6GlowActive, setD6GlowActive] = useState(false);
+    const [d4GlowActive, setD4GlowActive] = useState(false);
 
     const rollDice = (maxNumber) => {
 
@@ -20,6 +23,9 @@ const HexagonDice = () => {
         } else if (maxNumber == 4) {
             setD4Value("");
             setIsD4Spinning(true);
+        } else if (maxNumber == 6) {
+            setD6Value("");
+            setIsD6Spinning(true);
         }
         setTimeout(() => {
             const newValue = Math.floor(Math.random() * maxNumber) + 1; // Randomize dice value
@@ -29,6 +35,9 @@ const HexagonDice = () => {
             } else if (maxNumber == 4) {
                 setD4Value(newValue);
                 setIsD4Spinning(false);
+            } else if (maxNumber == 6) {
+                setD6Value(newValue);
+                setIsD6Spinning(false);
             }
         }, 1600); // Duration to match CSS animation
     };
@@ -36,21 +45,21 @@ const HexagonDice = () => {
     return (
 
         <div className="inline-flex">
-            <div class={`triangle-container ${isD4Spinning ? 'spinning' : ''}`}>
-                <div className={`hexagon-glow triangle  ${isD4Spinning ? 'no-glow' : ''} ${d4Active ? 'glow-active' : ''} ${d4Active ? 'triangle-active' : 'triangle-inactive'}`}
+            <div class={`triangle-container`}>
+                <div className={`triangle-glow triangle  ${isD4Spinning ? 'spinning' : ''} ${isD4Spinning ? 'no-glow' : ''} ${d4GlowActive ? 'glow-active' : ''} ${d4Active ? 'triangle-active' : 'triangle-inactive'}`}
                     onClick={() => rollDice(4)}
                 >
                     <span class="triangle-text"> {d4Value}</span>
                 </div>
             </div>
-            <div className={`hexagon-glow square ${isD6Spinning ? 'no-glow' : ''} ${d6Active ? 'glow-active' : ''} ${d6Active ? 'cube-active' : 'cube-inactive'}`}
+            <div className={`hexagon-glow square ${isD6Spinning ? 'spinning no-glow' : ''} ${d6GlowActive ? 'glow-active' : ''} ${d6Active ? 'square-active' : 'square-inactive'}`}
                 onClick={() => rollDice(6)}
             >
-                <span className="dice-text">{d6Value}</span>
+                <span className="square-text">{d6Value}</span>
             </div>
 
             <div
-                className={`hexagon-glow hexagon ${isD20Spinning ? 'spinning' : ''} ${isD20Spinning ? 'no-glow' : ''} ${d20Active ? 'glow-active' : ''} ${d20Active ? 'hexagon-active' : 'hexagon-inactive'}`}
+                className={`hexagon-glow hexagon ${isD20Spinning ? 'spinning' : ''} ${isD20Spinning ? 'no-glow' : ''} ${d20GlowActive ? 'glow-active' : ''} ${d20Active ? 'hexagon-active' : 'hexagon-inactive'}`}
                 onClick={() => rollDice(20)}
             >
                 {d20Value}
